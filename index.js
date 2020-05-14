3dcfd6b777dfeea9e260d0316e08509849014f4a
Michał Pastewski <michpastewski@gmail.com>
2020-05-14 18:13:16 +0200

    added cookie modal

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
+const cookie = document.querySelector('.cookie__hiden');
+const cookieButton = document.querySelector('.cookie__button');
+setTimeout(() => {
+     cookie.classList.remove('cookie__hiden');
+     cookie.classList.add('cookie');
+}, 1000);
+cookieButton.addEventListener('click', () => {
+     cookie.classList.remove('cookie');
+     cookie.classList.add('cookie__hiden');
+})
+
+
+
 const navigation = document.querySelector('.navigation');
 const burger = document.querySelector('.navigation__burger');
 const burgerLines = document.querySelectorAll('.navigation__burger--line');
 const navigationCheckbox = document.querySelector('#burger__checkbox');
 const navigationMenu = document.querySelector('.navigation__menu');
 
-
 burger.addEventListener('click', () => {
      if (!burgerLines[0].classList.contains('burger__active--one')) {
+          navigationCheckbox.setAttribute('disabled', 'disabled');
           navigationMenu.classList.remove('navigation__menu--close');
           navigationMenu.classList.add('navigation__menu--open');
-          navigationCheckbox.setAttribute('disabled', 'disabled');
           burgerLines[0].classList.add('burger__active--one');
           burgerLines[1].classList.add('burger__active--two');
           burgerLines[2].classList.add('burger__active--three');
-     } else 
-     if (burgerLines[0].classList.contains('burger__active--one')) {
+     } else if (burgerLines[0].classList.contains('burger__active--one')) {
           burgerLines[0].classList.remove('burger__active--one');
           burgerLines[1].classList.remove('burger__active--two');
           burgerLines[2].classList.remove('burger__active--three');
-          navigationMenu.classList.remove('navigation__menu--open');
           navigationMenu.classList.add('navigation__menu--close');
+          navigationMenu.classList.remove('navigation__menu--open');
           setTimeout(() => {
-               navigationMenu.classList.add('navigation__menu');
                navigationMenu.classList.remove('navigation__menu--close');
+               navigationMenu.classList.add('navigation__menu');
           }, 1000);
      }
 })
 
 navigationMenu.addEventListener('click', () => {
      if (navigationMenu.classList.contains('navigation__menu--open')) {
-          navigationMenu.classList.remove('navigation__menu--open');
-          navigationMenu.classList.add('navigation__menu--close');
           burgerLines[0].classList.remove('burger__active--one');
           burgerLines[1].classList.remove('burger__active--two');
           burgerLines[2].classList.remove('burger__active--three');
-          navigationCheckbox.removeAttribute('disabled');
+          navigationMenu.classList.remove('navigation__menu--open');
+          navigationMenu.classList.add('navigation__menu--close');
           setTimeout(() => {
-               navigationMenu.classList.add('navigation__menu');
                navigationMenu.classList.remove('navigation__menu--close');
+               navigationMenu.classList.add('navigation__menu');
           }, 1000);
      }
 })
