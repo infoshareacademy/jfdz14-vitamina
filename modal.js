07b9d71cbeaad2f8fe404026e25676ce74356b8c
Anna Rutkowska <rutkows.anna@gmail.com>
2020-06-03 17:05:29 +0200

    modal and validation

---
+++ modal.js
@@ -0,0 +1,54 @@
+/* modal window */
+
+const modalWindow = document.querySelector('.modal__overlay');
+const closeButton = document.querySelector('.modal__close');
+
+setTimeout(showModal, 1000);
+
+function closeModal() {
+    modalWindow.classList.remove("active");
+}
+function showModal() {
+    modalWindow.classList.add("active");
+}
+function outsideClick(e) {
+    if (e.target == modalWindow) {
+        closeModal();
+    }
+  }
+closeButton.addEventListener('click', () => {
+    closeModal();
+})
+
+window.addEventListener('click', outsideClick);
+
+/*email validation*/
+
+const email = document.querySelector('.modal__email');
+const form = document.querySelector('.modal__form');
+const emailReg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const emailErrorText = document.querySelector('.modal__email__error__text');
+const modalSubmit = document.querySelector('.modal__submit');
+
+modalSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+    checkEmail();
+});
+
+function checkEmail() {
+    const emailValue = (email.value.trim()).toLowerCase();
+    if (emailValue === '' || !emailReg.test(emailValue)) {
+        emailErrorText.classList.add('error');
+        email.classList.add('error');
+    }
+    else {
+        localStorage.setItem('email', emailValue);
+        window.open("/game.html");
+        closeModal();
+    }
+}
+
+form.addEventListener('focusin', (event) => {   
+    email.classList.remove("error");
+    emailErrorText.classList.remove("error");
+  });
