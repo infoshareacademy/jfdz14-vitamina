8e258b7f30aa0c2b37cff98f53bebfd02d6013b6
Michał Pastewski <michpastewski@gmail.com>
2020-06-06 14:24:15 +0200

    added scroll handler

--- index.js
+++ index.js
@@ -17,10 +17,13 @@ cookieButton.addEventListener('click', () => {
 })
 
 const navigation = document.querySelector('.navigation');
+const burgerInput = document.querySelector('.navigation__burger__checkbox');
 const burger = document.querySelector('.navigation__burger');
 const navigationMenu = document.querySelector('.navigation__menu');
 
 burger.addEventListener('click', () => {
+     burgerInput.checked = true;
+     console.log(burgerInput.checked)
      if (!navigation.classList.contains('active')) {
           navigation.classList.add('active');
      } else {
@@ -28,7 +31,26 @@ burger.addEventListener('click', () => {
      }
 })
 
-navigationMenu.addEventListener('click', () => {
-     navigation.classList.remove('active');
+// scroll handler
+
+const links = document.querySelectorAll('.navigation__menu__list__link');
+const pageSection = document.querySelectorAll('section');
+const headerSection = [...pageSection].filter((section) => section.getAttribute('id'))
+const nav = document.getElementsByClassName('navigation');
+let navOffsetHeight = nav[0].offsetHeight;
+
+// window.addEventListener('load', () => links.forEach((link) => link.removeAttribute('href')));
+
+links.forEach((link, index) => {
+     link.addEventListener('click', () => {
+          link.removeAttribute('href');
+          navigation.classList.remove('active');
+          let sectionOffsetTop = headerSection[index].offsetTop;
+          let scrollTo = sectionOffsetTop - navOffsetHeight;
+          window.scrollTo({
+               top: scrollTo,
+               behavior: 'smooth'
+          });
+     });
 })
 
