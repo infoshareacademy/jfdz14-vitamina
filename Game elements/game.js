73b9df35857ef2f240e899e9490cbde51baab74a
Mikołaj Jędrak <61053998+KubaJedrak@users.noreply.github.com>
2020-06-13 15:11:18 +0200

    working on ranking and scoring

--- Game elements/game.js
+++ Game elements/game.js
@@ -4,6 +4,7 @@ const secondScreen = document.querySelector('.game__box');
 class Food {
      positionX = 10;
      positionY = 5;
+     number = 0;
 
      createFoodContainer() {
           this.food = document.createElement('div');
@@ -13,17 +14,19 @@ class Food {
      randomPosition() {
           this.positionY = Math.random() * 80;
           this.food.style.bottom = this.positionY + 2 +"%";
-          this.positionY;
+          this.positionY; // dlaczego tak ^ ??
      }
 
      runToLeft() {
           setInterval(() => {
-               if (this.positionX >= 95) {
+               if (this.positionX > 95) { // removing = from >=
                     this.food.remove();
                }
                if (this.positionX <= 100) {
                     this.food.style.left = this.positionX + "%";
                     this.positionX += .4;
+
+                    // console.log(this.positionY + "||" + this.positionX)
                }
           }, 50);
      }
@@ -47,11 +50,11 @@ class Food {
           if (number > 50 && number < 80) {
                this.food.classList.add('game__box--badfood');
                this.food.style.backgroundImage = badFood[choiceFood];
-          } else if (number > 20 && number < 50){
+          } else if (number > 20 && number < 50){ // 50 jest nieużywane
                this.food.classList.add('game__box--goodfood');
                this.food.style.backgroundImage = goodFood[choiceFood];
           } else {
-               this.food.style.backgroundImage = '';
+               this.food.style.backgroundImage = ''; // a czemu puste? One wciąż lecą 
           };
      }
 
@@ -62,29 +65,37 @@ class Food {
           this.runToLeft();
      }
 }
+
 class Human {
-     positionX = 0;
+     positionX = 100;
      positionY = 0;
-     width = 51;
+     width = 51; // dajmy mu większą szerokość dla łątwiejszego sterowania?
      height = 120;
 
      createHumanElement() {
           this.human = document.createElement('div');
           this.human.classList.add('human');
           secondScreen.appendChild(this.human);
+
+          // //adding a "food catcher"
+          // this.maw = document.createElement('div');
+          // this.maw.classList.add("maw")
+          // this.human.appendChild(this.maw);
      }
      humanMove() {
           let sreenTopPosition = secondScreen.offsetTop;
 
           this.positionY = event.clientY - sreenTopPosition - this.width / 2;
 
-          if (this.positionY >= 400) {
-               this.positionY = 400;
+          if (this.positionY >= 395) {
+               this.positionY = 395; //zmieniona bo był clip za ramy (zmienic min wysokosc spawnu zarcia?)
           }
-          if (this.positionY <= 1 ) {
-               this.positionY = 1;
+          if (this.positionY <= 30 ) { 
+               this.positionY = 30; //zmienione na przestrzen na staty
           }
           this.human.style.top = this.positionY + 'px';
+
+          // console.log(this.positionY + "||" + this.positionX) // remove later
      }
      humanPositionY() {
           this.human.addEventListener('mousemove', () => this.humanMove());
@@ -97,12 +108,84 @@ const randomFoodCreate = () => {
           food.initializeFood();
      }, 1000);
 }
+
 const human = new Human();
 human.createHumanElement();
 human.humanPositionY();
 randomFoodCreate();
 
+let food = new Food();
+
+// -----------------------------------------------------------------------------------
+
+let lives = 5;
+let time = 0;
+let score = 0;
+
+let timer = document.querySelector(".game__timer");
+
+timer.textContent = time;
+
+const removeHeart = () => {
+     const hearts = document.querySelector(".game__lives");
+     
+     console.log(hearts.lastChild)
+          
+     hearts.lastChild.remove();   // działa co drugi raz? WTF
+}
+
+const gameTime = () => {
+          setInterval( () => {
+               time += 0.1;   
+
+               return (Math.round((time + Number.EPSILON) * 100) / 100)
+          }, 100)
+     }
+
+const gameScore = () => {
+     return score += 1;   // start z 1 ??? ;/
+}
+
+const updateStats = () => { //niepotrzebne?
+     
+     gameTime()
+     removeHeart()
+}
+
+const gameStop = () => {
+
+}
+
+const detectDevouring = () => {
+     if (food.positionX == human.positionX && food.positionY >= human.positionY - 30 || food.positionY <= human.positionY + 30) {
+          if (food.number > 20 && food.number < 50) { // >=  czy >
+               gameScore()
+          } else if (food.number >50 && food.number < 80) {
+               removeHeart();
+               if (removeHeart() = undefined) {
+                    gameStop(); // nie wiem, czy to tu?
+               }
+          }
+          return score;       
+     }
+     return score;
+}
+
+detectDevouring();
+
+
+// dodajmy max-width lub costam bo background sie przycina przy wiekszych 1x1
 
 
 
 
+
+
+
+
+
+// --- \/ test \/ ---
+
+
+// --- \/ test \/ ---
+        //  console.log(maw.positionY) // remove later
